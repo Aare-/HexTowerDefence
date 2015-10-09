c8bb6407535467481561ded52ed74a2081e5ba11
fblost <fblost@gmail.com>
2015-10-09 21:00:29 +0200

    Initial commit. Libgdx project setup. Design doc setup.

---
+++ game/desktop/src/com/subfty/tg/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.subfty.tg.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.subfty.tg.TGMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TGMain(), config);
+	}
+}
