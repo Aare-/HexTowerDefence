11d89fe4ca19da49c7d3f756f808c7e29590f733
fblost <fblost@gmail.com>
2015-10-19 12:47:12 +0200

    Added Entitas library

---
+++ code/Assets/Generated/ComponentIds.cs
@@ -0,0 +1,10 @@
+public static class ComponentIds {
+
+    public const int TotalComponents = 0;
+
+    public static readonly string[] componentNames = {
+    };
+
+    public static readonly System.Type[] componentTypes = {
+    };
+}
