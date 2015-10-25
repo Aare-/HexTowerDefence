918b4ce5c550bf4cda4e6a2b148cc2b65b5e06c3
fblost <fblost@gmail.com>
2015-10-25 02:13:31 +0100

    Started enemies

---
+++ code/Assets/Scripts/Components/Enemy.cs
@@ -0,0 +1,9 @@
+﻿using Entitas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Enemy : IComponent {
+
+}
---
+++ code/Assets/Scripts/Features/Enemies/MoveEnemiesSystem.cs
@@ -0,0 +1,21 @@
+﻿using Entitas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class MoveEnemiesSystem : IExecuteSystem, ISetPool {    
+    Group _group;
+
+    public void SetPool(Pool pool) {
+        //_group = pool.GetGroup(Matcher.AllOf(Matcher.Ene));
+    }
+
+    public void Execute() {
+        /*foreach (var e in _group.GetEntities()) {
+            var move = e.move;
+            var pos = e.position;
+            e.ReplacePosition(pos.x, pos.y + move.speed, pos.z);
+        }*/
+    }
+}
